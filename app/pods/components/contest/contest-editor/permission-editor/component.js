e70afa96f00bd3518d21fa526a5eff988148307e
Jatin Katyal <jatin.katyal13@gmail.com>
2020-07-20 19:34:01 +0530

    complete permission editor

--- app/pods/components/contest/contest-editor/permission-editor/component.js
+++ app/pods/components/contest/contest-editor/permission-editor/component.js
@@ -1,5 +1,12 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
 export default class ContestContestEditorPermissionEditorComponent extends Component {
   showRegistrationForm = false
+  @tracked limitedAttempt = false
+
+  constructor() {
+    super(...arguments);
+    this.limitedAttempt = this.args.contest.max_attempts || this.args.contest.duration;
+  }
 }
